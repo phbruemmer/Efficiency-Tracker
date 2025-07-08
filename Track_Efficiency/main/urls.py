d996c54f5091280a5aa95fa473201355f0d5a38c
phbruemmer <phbruemmer@gmail.com>
2025-07-08 22:46:10 +0200

    - register and login now working, related changes in the template and static -files

--- Track_Efficiency/main/urls.py
+++ Track_Efficiency/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import tracker_view
+from .views import tracker_view, register_view, login_view
 
 urlpatterns = [
+    path('register/', register_view, name='register_view'),
+    path('login/', login_view, name='login_view'),
     path("", tracker_view, name="tracker_view"),
 ]
--- Track_Efficiency/main/views.py
+++ Track_Efficiency/main/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
 
 
 """
@@ -12,7 +16,47 @@ The new version will appear in a new repository on GitHub.
 """
 
 
-def register()
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirmPassword']
+
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match.")
+            return redirect('register_view')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+            return redirect('register_view')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already in use.")
+            return redirect('register_view')
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        messages.success(request, "Account created successfully.")
+        return redirect('tracker_view')
+    return render(request, 'register.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('tracker_view')
+        else:
+            pass
+            # messages.error(request, 'Invalid username or password')
+    return render(request, "login.html")
 
 
 def tracker_view(request):
